81ef731b2037efa6efa1fd0bd55ec499e5d307c0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-01-26 18:18:44 +0400

    Add DB_DSN inside .env and .env.dist

--- server/db.js
+++ server/db.js
@@ -1,11 +1,23 @@
 const mysql = require('mysql');
-console.log(process.env.DB_PASS);
+
+let database, port, host;
+if (process.env.DB_DSN){
+  const matches = process.env.DB_DSN.match(/host=([^;]+);port=(\d+);dbname=(.+)/);
+  host = matches[1];
+  port = matches[2];
+  database = matches[3];
+} else {
+  host = process.env.DB_HOST;
+  port = process.env.DB_PORT;
+  database = process.env.DB_NAME;
+}
 
 var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: host,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: database,
+  port: port
 });
 
 connection.connect();
